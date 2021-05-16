b8c100eafedddb54fa3c9fb0b9df0e89c35f3ec3
billibon80 <78269170+billibon80@users.noreply.github.com>
2021-05-16 22:00:50 +0300

    Add files via upload

---
+++ post.py
@@ -0,0 +1,9 @@
+
+class Post:
+
+    def __init__(self, post_date, post_id, title, subtitle, body):
+        self.post_date = post_date
+        self.post_id = post_id
+        self.title = title
+        self.subtitle = subtitle
+        self.body = body
